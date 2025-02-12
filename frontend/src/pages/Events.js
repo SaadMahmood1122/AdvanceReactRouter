32f631dd97f8d8706bf2c6d655c40ba3a1235155
SaadMahmood1122 <sk8066709@gamil.com>
2025-02-12 19:38:19 +0500

    added advance error handling / Generic error handling

--- frontend/src/pages/Events.js
+++ frontend/src/pages/Events.js
@@ -2,8 +2,8 @@ import EventsList from "../components/EventsList";
 import { useLoaderData } from "react-router-dom";
 function EventsPage() {
   const data = useLoaderData();
-  if (data.isError === true) return <p>{data.message}</p>;
-  else return <EventsList events={data.events} />;
+  const events = data.events;
+  return <EventsList events={events} />;
 }
 
 export default EventsPage;
